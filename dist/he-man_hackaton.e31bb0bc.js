d4e3f402324bc56675da0c09765f74566fb4115e
Odd Karsten Svartaas <odd.karsten.svartaas@dnb.no>
2019-11-22 10:42:47 +0100

    Cleaning shit

--- dist/he-man_hackaton.e31bb0bc.js
+++ dist/he-man_hackaton.e31bb0bc.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62326" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62595" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
