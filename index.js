5bb7874789831b40bc053ddecaa770231ecc4bf0
Thomas <tsn1985@gmail.com>
2019-11-22 11:31:53 +0100

    Add navigation menu

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Skeletor sucks.");
+
+let map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+    console.log("Set up map: ", map)
+}
+window.onload = initMap();
